5c9a7dfea050f4222438d3188e040947a946a630
Aleksandr Mukhamedov <3a2nok@gmail.com>
2019-08-02 07:57:52 +0300

    test commit 2

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,9 +7,13 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+        if (size() != storage.length) {
+                    storage[size()] = r;
+        }
     }
 
     Resume get(String uuid) {
@@ -21,10 +27,20 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] all = new Resume[size()];
+        for (int i = 0; i < all.length - 1; i++) {
+                all[i] = storage[i];
+        }
+        return all;
     }
 
     int size() {
-        return 0;
+        int storageSize = 0;
+        for (int i = 0; i < storage.length - 1; i++) {
+            if (storage[i] != null) {
+                storageSize++;
+            }
+        }
+        return storageSize;
     }
 }
