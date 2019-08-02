e284a8b3714aa73092eb0aebf87d59703900da19
Aleksandr Mukhamedov <3a2nok@gmail.com>
2019-08-02 22:45:29 +0300

    finished code

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -11,16 +11,42 @@ public class ArrayStorage {
     }
 
     void save(Resume r) {
-        if (size() != storage.length) {
+        if (size() == 0) {
+            storage[size()] = r;
+        }
+        else if (size() != storage.length) {
+            for (int i = 0; i <= size() - 1; i++) {
+                if (r.uuid.equals(storage[i].uuid)) {
+                    break;
+                }
+                else if (i == size() - 1) {
                     storage[size()] = r;
+                }
+            }
         }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume get = null;
+        if (size() != 0) {
+            for (int i = 0; i <= size() - 1; i++) {
+                if (storage[i].uuid.equals(uuid)) {
+                    get = storage[i];
+                    break;
+                }
+            }
+        }
+        return get;
     }
 
     void delete(String uuid) {
+        if (size() != 0) {
+            for (int i = 0; i <= size() - 1; i++) {
+                if (storage[i].uuid.equals(uuid)) {
+                    if (size() - i >= 0) System.arraycopy(storage, i + 1, storage, i, size() - i);
+                }
+            }
+        }
     }
 
     /**
@@ -28,19 +54,19 @@ public class ArrayStorage {
      */
     Resume[] getAll() {
         Resume[] all = new Resume[size()];
-        for (int i = 0; i < all.length - 1; i++) {
+        for (int i = 0; i <= all.length - 1; i++) {
                 all[i] = storage[i];
         }
         return all;
     }
 
     int size() {
-        int storageSize = 0;
+        int resumeAmount = 0;
         for (int i = 0; i < storage.length - 1; i++) {
             if (storage[i] != null) {
-                storageSize++;
+                resumeAmount++;
             }
         }
-        return storageSize;
+        return resumeAmount;
     }
 }
