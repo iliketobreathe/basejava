17a4cf0edb53f66bacea1f6d75f1580d755a45d0
Aleksandr Mukhamedov <3a2nok@gmail.com>
2019-08-05 21:07:02 +0300

    small changes in get method

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -13,13 +13,11 @@ public class ArrayStorage {
     void save(Resume r) {
         if (size() == 0) {
             storage[size()] = r;
-        }
-        else if (size() != storage.length) {
+        } else if (size() != storage.length) {
             for (int i = 0; i <= size() - 1; i++) {
                 if (r.uuid.equals(storage[i].uuid)) {
                     break;
-                }
-                else if (i == size() - 1) {
+                } else if (i == size() - 1) {
                     storage[size()] = r;
                 }
             }
@@ -27,11 +25,13 @@ public class ArrayStorage {
     }
 
     Resume get(String uuid) {
-        Resume requestedResume = null;
+        Resume requestedResume = new Resume();
+        requestedResume.uuid = null;
+
         if (size() != 0) {
             for (int i = 0; i <= size() - 1; i++) {
                 if (storage[i].uuid.equals(uuid)) {
-                    requestedResume = storage[i];
+                    requestedResume.uuid = uuid;
                     break;
                 }
             }
@@ -55,7 +55,7 @@ public class ArrayStorage {
     Resume[] getAll() {
         Resume[] all = new Resume[size()];
         for (int i = 0; i <= all.length - 1; i++) {
-                all[i] = storage[i];
+            all[i] = storage[i];
         }
         return all;
     }
