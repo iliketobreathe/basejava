6f963a41942c21da69bd3afdea9539ff5b39434d
Aleksandr Mukhamedov <3a2nok@gmail.com>
2019-08-06 21:15:07 +0300

    error correction

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -8,10 +8,8 @@ public class ArrayStorage {
 
     void clear() {
         int size = size();
-        if (size != 0) {
-            for (int i = 0; i < size; i++) {
-                storage[i] = null;
-            }
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
         }
     }
 
