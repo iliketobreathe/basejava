6d21bdc92da98dc495489952959f8687706cda8c
Aleksandr Mukhamedov <3a2nok@gmail.com>
2019-08-06 23:38:09 +0300

    error correction

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -8,7 +8,6 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
-        /*int size = size();*/
         for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
@@ -16,7 +15,6 @@ public class ArrayStorage {
     }
 
     void save(Resume r) {
-        /*int size = size();*/
         if (size != storage.length && r.uuid != null) {
             for (int i = 0; i < size; i++) {
                 if (r.uuid.equals(storage[i].uuid)) {
@@ -29,38 +27,30 @@ public class ArrayStorage {
     }
 
     Resume get(String uuid) {
-   /*     int size = size();*/
-        if (size != 0) {
             for (int i = 0; i <= size - 1; i++) {
                 if (storage[i].uuid.equals(uuid)) {
                     return  storage[i];
                 }
             }
-        }
         return null;
     }
 
     void delete(String uuid) {
-      /*  int size = size();*/
-        if (size != 0) {
             for (int i = 0; i < size; i++) {
                 if (storage[i].uuid.equals(uuid)) {
-                        System.arraycopy(storage, i + 1, storage, i, size - i);
+                        System.arraycopy(storage, i + 1, storage, i, size - 1 - i);
                         size--;
                         return;
                 }
             }
-        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-   /*     int size = size();*/
         Resume[] resumes = new Resume[size];
         if (size > 0) {
-            resumes = new Resume[size];
             System.arraycopy(storage, 0, resumes, 0, size);
         }
         return resumes;
@@ -68,12 +58,5 @@ public class ArrayStorage {
 
     int size() {
         return size;
-/*        int resumeAmount = 0;
-        for (int i = 0; i < storage.length - 1; i++) {
-            if (storage[i] != null) {
-                resumeAmount++;
-            }
-        }
-        return resumeAmount;*/
     }
 }
