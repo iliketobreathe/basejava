2b923932a83fd42a3c9f0b24d77d6dda4ea8f42e
Aleksandr Mukhamedov <3a2nok@gmail.com>
2019-08-15 21:34:43 +0300

    fixes

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,42 +5,27 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     private int size = 0;
-    private int storageElementNumber = 0;
+    private int index = 0;
     private Resume[] storage = new Resume[10_000];
 
-/*    private boolean isInStorage(Resume resume) {
-        storageElementNumber = 0;
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(resume.uuid)) {
-                storageElementNumber++;
-                return true;
-            }
-        }
-        return false;
-    }*/
-
-    private boolean isExist(String uuid) {
-        storageElementNumber = 0;
+    private int isExist(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                storageElementNumber = i;
-                return true;
+                index = i;
+                return i;
             }
         }
-        return false;
+        return -1;
     }
 
     public void clear() {
-        /*for (int i = 0; i < size; i++) {
-            storage[i] = null;
-        }*/
-        Arrays.fill(storage, 0, size - 1, null);
+        Arrays.fill(storage, 0, size - 2, null);
         size = 0;
     }
 
     public void update(Resume resume) {
-        if (isExist(resume.uuid)) {
-            storage[storageElementNumber] = resume;
+        if (isExist(resume.uuid) >= 0) {
+            storage[index] = resume;
             System.out.println("Resume with uuid " + resume.uuid + " is found in storage and updated");
             return;
         }
@@ -49,29 +34,29 @@ public class ArrayStorage {
 
     public void save(Resume resume) {
         if (size != storage.length && resume.uuid != null) {
-            if (isExist(resume.uuid)) {
+            if (isExist(resume.uuid) >= 0) {
                 System.out.println("Resume with this uuid is already in storage");
                 return;
             }
             storage[size] = resume;
             size++;
-        }
-        else {
+        } else {
             System.out.println("ERROR: Storage is full or you entered empty uuid");
         }
     }
 
     public Resume get(String uuid) {
-        if (isExist(uuid)) {
-            return  storage[storageElementNumber];
+        if (isExist(uuid) >= 0) {
+            return  storage[index];
         }
         System.out.println("ERROR: Resume with this uuid isn't in storage");
         return null;
     }
 
     public void delete(String uuid) {
-        if (isExist(uuid)) {
-            System.arraycopy(storage, storageElementNumber + 1, storage, storageElementNumber, size - 1 - storageElementNumber);
+        if (isExist(uuid) >= 0) {
+            System.arraycopy(storage, index + 1, storage, index, size - 1 - index);
+            storage[size - 1] = null;
             size--;
             return;
         }
@@ -82,11 +67,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        Resume[] resumes = new Resume[size];
-        if (size > 0) {
-            System.arraycopy(storage, 0, resumes, 0, size);
-        }
-        return resumes;
+        return Arrays.copyOf(storage, size);
     }
 
     public int size() {
