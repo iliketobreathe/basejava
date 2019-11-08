61f77fa3739df4efe94725bddd401adf2f4dc7da
Aleksandr Mukhamedov <3a2nok@gmail.com>
2019-11-08 18:15:35 +0300

    HW_12_FIXES_01
    1. MainStreams - refactored methods minValue and oddOrEven

--- src/ru/basejava/iliketobreathe/MainStreams.java
+++ src/ru/basejava/iliketobreathe/MainStreams.java
@@ -2,54 +2,32 @@ package ru.basejava.iliketobreathe;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class MainStreams {
     public static void main(String[] args) {
         int[] array = {1, 4, 3, 1, 5, 4, 1, 3};
-        List<Integer> list = new ArrayList<>();
-        list = Arrays.asList(1, 4, 3, 1, 5, 4, 1, 3);
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 4, 3, 1, 5, 4, 1, 3));
 
         System.out.println(minValue(array));
-
         System.out.println(oddOrEven(list));
     }
 
     private static int minValue(int[] values) {
-        var ref = new Object() {
-            int i = 0;
-        };
-
-        AtomicInteger result = new AtomicInteger();
-
-        Arrays.stream(values).boxed().sorted(Collections.reverseOrder()).distinct().forEach(x -> {
-            int pos = (int)Math.pow(10, ref.i++);
-            result.addAndGet(x * pos);
-        });
-
-        return result.get();
+        return Arrays.stream(values).sorted().distinct().reduce((x, y) -> x * 10 + y).orElse(0);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
-
-        List<Integer> newIntegers = new ArrayList<>();
+        List<Integer> newIntegers;
 
         int sum = integers.stream().mapToInt(Integer::intValue).sum();
 
-        integers.stream().forEach(x -> {
-            if (sum % 2 == 0) {
-                if (x % 2 == 0) {
-                    newIntegers.add(x);
-                }
-            } else {
-                if (x % 2 != 0) {
-                    newIntegers.add(x);
-                }
-            }
-
-        });
+        if (sum % 2 == 0) {
+            newIntegers = integers.stream().filter(x -> x % 2 != 0).collect(Collectors.toList());
+        } else {
+            newIntegers = integers.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
+        }
 
         return newIntegers;
     }
